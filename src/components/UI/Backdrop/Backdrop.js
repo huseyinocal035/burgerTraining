f14f53a01390011588fc7b566f1084cfc91f0c37
huseyinocal035 <huseyinocal035@gmail.com>
2020-09-16 18:02:16 +0300

    Backdrop component implemented

--- src/components/UI/Backdrop/Backdrop.js
+++ src/components/UI/Backdrop/Backdrop.js
@@ -1,10 +1,10 @@
-// import React from 'react';
-//
-// const backdrop = (props) => (
-//     <div>
-//         {props.children}
-//     </div>
-//
-// )
-//
-// export default backdrop;
+import React from 'react';
+import classes from './Backdrop.css'
+
+
+const backdrop = (props) => (
+    props.show ? <div className={classes.Backdrop} onClick={props.clicked}></div> : null
+
+)
+
+export default backdrop;
