7a5386d58cd6a947ff7d1b97638b06baa5dae29f
huseyinocal035 <huseyinocal035@gmail.com>
2020-09-16 18:20:37 +0300

    BurgerTraining- Custom button and total price added and implemented.

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -75,6 +75,10 @@ class BurgerBuilder extends Component{
         this.setState({purchasing: false})
     }
 
+    purchaseContinueHandler = () => {
+        alert('You continue')
+    }
+
     render() {
         const disabledInfo = {
             ...this.state.ingredients
@@ -85,7 +89,12 @@ class BurgerBuilder extends Component{
         return(
             <Auxh>
                 <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
-                    <OrderSummary ingredient={this.state.ingredients} />
+                    <OrderSummary
+                        ingredient={this.state.ingredients}
+                        price={this.state.totalPrice.toFixed(2)}
+                        purchaseCanceled={this.purchaseCancelHandler}
+                        purchaseContinued={this.purchaseContinueHandler}
+                    />
                 </Modal>
                 <Burger ingredients={this.state.ingredients} />
                 <BuildControls
