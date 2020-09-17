a6e202f0417e2aff4a02f71fada0351c31747eb1
huseyinocal035 <huseyinocal035@gmail.com>
2020-09-17 17:55:03 +0300

    Some refactor

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,15 +1,38 @@
-import React from "react";
-import Auxh from '../../hoc/Auxh'
-import classes from './Layout.css'
-import Toolbar from '../Navigation/Toolbar/Toolbar'
+import React, { Component } from 'react';
 
-const layout = (props) => (
-    <Auxh>
-        <Toolbar/>
-        <main className= {classes.Content}>
-            {props.children}
-        </main>
-    </Auxh>
-)
+import Auxh from '../../hoc/Auxh';
+import classes from './Layout.css';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-export default layout
+class Layout extends Component {
+    state = {
+        showSideDrawer: false
+    }
+
+    sideDrawerClosedHandler = () => {
+        this.setState( { showSideDrawer: false } );
+    }
+
+    sideDrawerToggleHandler = () => {
+        this.setState( ( prevState ) => {
+            return { showSideDrawer: !prevState.showSideDrawer };
+        } );
+    }
+
+    render () {
+        return (
+            <Auxh>
+                <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler} />
+                <SideDrawer
+                    open={this.state.showSideDrawer}
+                    closed={this.sideDrawerClosedHandler} />
+                <main className={classes.Content}>
+                    {this.props.children}
+                </main>
+            </Auxh>
+        )
+    }
+}
+
+export default Layout;
